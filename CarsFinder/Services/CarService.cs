e79cf84e3fa9b99ca653e8bccf1ee7b86e8d2cb7
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-20 16:01:55 +0300

    Create Car service with method that get all cars

---
+++ CarsFinder/Services/CarService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using CarsFinder.Model;
+
+namespace CarsFinder.Services;
+
+class CarService
+{
+    private const string CarsFilePath = "../../../Input/cars.json";
+
+    public async Task<IEnumerable<Car>> GetCars()
+    {
+        using var stream = await FileSystem.OpenAppPackageFileAsync(CarsFilePath);
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+
+        var cars = JsonSerializer.Deserialize<IEnumerable<Car>>(contents);
+
+        return cars;
+    }
+}
