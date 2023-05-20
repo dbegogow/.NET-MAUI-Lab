f0a2e729f888a17149471f8b26969b40476fc166
Dzhulio Begogov <dzhulio.begogov@gmail.com>
2023-05-20 15:01:32 +0300

    Create Car model

---
+++ CarsFinder/Model/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace CarsFinder.Model;
+
+public class Car
+{
+    public string Name { get; set; }
+
+    public int Year { get; set; }
+
+    public int Horsepower { get; set; }
+
+    public int Doors { get; set; }
+
+    public int EngineCC { get; set; }
+
+    public string FuelType { get; set; }
+
+    public string StoreLocation { get; set; }
+
+    public string Image { get; set; }
+}
